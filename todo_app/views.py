01fcc48b62e92876810ac36f015e88f36ac87706
shubham114111 <sshubham681@gmail.com>
2021-01-20 21:32:42 +0530

    Signup improvements!

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError 
 
 def usersignup(request):
-    return render(request,'todo_app/usersignup.html',{'form':UserCreationForm})
+    if request.method == 'GET':
+        return render(request,'todo_app/usersignup.html',{'form':UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password =request.POST['password1'])
+                user.save()
+            except IntegrityError:
+                return render(request, 'todo_app/usersignup.html',{'form':UserCreationForm(),'error':'This username has already been taken, Try with different username.'})
+        else:
+            return render(request, 'todo_app/usersignup.html',{'form':UserCreationForm(),'error': "Password didn't match "})
